a819f67901be155fcfffcfc90b4e4238ecae857e
AlonsoMarreroBello <usr3891@salesianos-lacuesta.net>
2025-05-17 12:42:58 +0100

    feat(): Created custom table for information display.

---
+++ src/interfaces/CustomTable.interface.ts
@@ -0,0 +1,38 @@
+export interface BaseDataRow {
+  id: string | number;
+  [key: string]: string | number | boolean | null | undefined;
+}
+
+export interface DataColumnDefinition<T extends BaseDataRow> {
+  type: "data";
+  field: keyof T;
+  headerName: string;
+  width?: number | string;
+  sortable?: boolean;
+  renderCell?: (value: T[keyof T], row: T) => React.ReactNode;
+}
+
+export interface ActionColumnDefinition<T extends BaseDataRow> {
+  type: "actions";
+  headerName?: string;
+  width?: number | string;
+  renderActions: (row: T) => React.ReactNode;
+}
+
+// Tipo Unión para las columnas
+export type TableColumn<T extends BaseDataRow> =
+  | DataColumnDefinition<T>
+  | ActionColumnDefinition<T>;
+
+export interface CustomTableProps<T extends BaseDataRow> {
+  columns: TableColumn<T>[];
+  data: T[];
+  initialPageSize?: number;
+  pageSizeOptions?: number[];
+  onRowClick?: (id: string | number) => void;
+}
+
+export interface SortConfig<T extends BaseDataRow> {
+  key: keyof T | null;
+  direction: "ascending" | "descending";
+}
